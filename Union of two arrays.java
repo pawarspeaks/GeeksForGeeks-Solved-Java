9cf9c01f2e75f0a37fbe124f8f6f3eb05bcd8796
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-02-27 22:24:58 +0530

    Create Union of two arrays.java

---
+++ Union of two arrays.java
@@ -0,0 +1,14 @@
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        //Your code here
+        HashSet<Integer> s = new HashSet<Integer>();
+        for(int i=0;i<n;i++) { 
+            s.add(a[i]);
+        }
+        for(int i=0;i<m;i++) {
+            s.add(b[i]);
+        }
+        return s.size();
+    }
+}
