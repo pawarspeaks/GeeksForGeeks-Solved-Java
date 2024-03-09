bb9e441220576f4aed5f849efd4952f0269f213d
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-03-09 22:26:55 +0530

    Update

---
+++ Maximum Product Subarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Function to find maximum product subarray
+    long maxProduct(int[] arr, int n) {
+        // code here
+  long prefix = 1;
+        long suffix = 1;
+        long ans = Integer.MIN_VALUE;
+
+
+        for(int i = 0; i < n; i++) {
+            if(prefix == 0) {
+                prefix = 1;
+            }
+            if(suffix == 0) {
+                suffix = 1;
+            }
+            
+            prefix = prefix*arr[i];
+            suffix = suffix*arr[n-i-1];
+            ans = Math.max(ans, Math.max(prefix, suffix));
+        }
+
+
+        return ans;
+  
+    }
+}
