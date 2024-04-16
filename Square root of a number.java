8393035eac2b61b2501b6e746281112cb6a58263
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-04-16 22:23:12 +0530

    Create Square root of a number.java

---
+++ Square root of a number.java
@@ -0,0 +1,10 @@
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		 return (long)Math.floor(Math.sqrt(x));
+	 }
+}
