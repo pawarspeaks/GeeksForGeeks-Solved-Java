5bd66a294822b97004f464a2b604d8c1125556ef
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-04-25 23:45:05 +0530

    Create Maximum sum of hour glass.java

---
+++ Maximum sum of hour glass.java
@@ -0,0 +1,27 @@
+class Solution {
+    int findMaxSum(int n, int m, int mat[][]) {
+        // code here
+       if(m < 3 || n < 3) return -1;
+        int max = Integer.MIN_VALUE;
+        int maxi = 0;
+        int maxj = 0;
+        n-=2;
+        m-=2;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                int sum = mat[i][j] + mat[i][j+1] + mat[i][j+2];
+                sum = sum + mat[i+1][j+1];
+                sum = sum + mat[i+2][j] + mat[i+2][j+1] + mat[i+2][j+2];
+                if(sum > max){ 
+                    max = sum;
+                    maxi = i;
+                    maxj = j;
+                }
+            }
+        }
+        // System.out.println(mat[maxi][maxj] + " " + mat[maxi][maxj+1] + " " + mat[maxi][maxj+2]);
+        // System.out.println("   "+ mat[maxi+1][maxj+1]);
+        // System.out.println(mat[maxi+2][maxj] + " " + mat[maxi+2][maxj+1] + " " + mat[maxi+2][maxj+2]);
+        return max;
+    }
+};
