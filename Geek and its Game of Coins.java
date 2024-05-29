9a52ddd74b07d93903891f91144be40053aefdf3
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-05-29 22:45:43 +0530

    Create Geek and its Game of Coins.java

---
+++ Geek and its Game of Coins.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int findWinner(int n, int x, int y) {
+        // code here
+          int[] dp = new int[n + 1]; 
+        dp[1] = 1; 
+
+        for (int i = 2; i <= n; i++) { 
+            if ((i - 1 >= 0 && dp[i - 1] == 0) || 
+                (i - x >= 0 && dp[i - x] == 0) || 
+                (i - y >= 0 && dp[i - y] == 0)) 
+                dp[i] = 1; 
+            else
+                dp[i] = 0; 
+        } 
+        return dp[n] == 1 ? 1 : 0;
+    }
+}
